291658a7b62cc3e35f696ae09378701cfbf20355
valeraqwe <valera.komar.1999@gmail.com>
2023-06-08 18:26:15 +0200

    some fix 2

--- form-handler.php
+++ form-handler.php
@@ -1,4 +1,5 @@
 <?php
+
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
@@ -6,17 +7,16 @@ $message = $_POST['message'];
 
 $email_from = 'trump@2024.com';
 
-$email_subject = 'New Form Submission'
+$email_subject = 'New Form Submission';
 
-$email_body = "User Name: $name.\n".
-               "User Email: $visitor_email.\n".
-                "Subject: $subject.\n".
-                 "User Message: $message .\n";
+$email_body = "User Name: $name.\n" .
+    "User Email: $visitor_email.\n" .
+    "Subject: $subject.\n" .
+    "User Message: $message .\n";
  $to = 'valera.komar.1999@gmail.com';
  
  $headers = "From: $email_from \r\n";
  $headers .= "Reply-To: $visitor_email \r\n";
 
- mail($to,$email_subject,$email_body,$headers);
+ mail($to, $email_subject, $email_body, $headers);
 header("Location: contact.html");
-?>
